f9ed930b38bd79320b454a11f622ec30be4b2cb1
Lou1sM <lmahonology@gmail.com>
2024-05-17 14:08:01 +0100

    add recall to example_prefs.py

--- example_prefs.py
+++ example_prefs.py
@@ -1,14 +1,25 @@
 from prefs.atomic_facts import AtomicFactGenerator
 from prefs.factscorer import FactScorer
 
-afg=AtomicFactGenerator()
-example_text = 'Jim is the man who works at the shop. Bob also works at the shop. Mick has a cat.'
-facts_and_sources = afg.extract_facts(example_text) # list of (sent, facts) tuples
-facts = [x for item in facts_and_sources for x in item[1]]
 
+example_output = 'Jim is the man who works at the shop. Bob also works at the shop. Mick has a cat.'
 gold_summary = 'Jim and Bob work at the shop.'
+
+afg=AtomicFactGenerator()
+predicted_facts_and_sources = afg.extract_facts(example_output) # list of (sent, facts) tuples
+predicted_facts = [x for item in predicted_facts_and_sources for x in item[1]]
+gold_facts_and_sources = afg.extract_facts(gold_summary) # list of (sent, facts) tuples
+gold_facts = [x for item in gold_facts_and_sources for x in item[1]]
+
 fs = FactScorer(cache_dir_prefix='.')
-prefs_score, score_per_fact = fs.get_score(facts,
-                                           gold_summary,
+fact_precision, score_per_fact = fs.get_score(predicted_facts,
+                                              gold_summary,
+                                              summname='test-summary',
+                                              )
+
+fact_recall, score_per_fact = fs.get_score(gold_facts,
+                                           example_output,
                                            summname='test-summary',
                                            )
+
+prefs_score = (2*fact_precision*fact_recall) / (fact_precision+fact_recall)
